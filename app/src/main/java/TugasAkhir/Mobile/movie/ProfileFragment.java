62782e316668c50b8e507d3f4601d4a51e08d5ef
Ganis Prabandari <61128240+ganisdwpr@users.noreply.github.com>
2021-06-19 19:00:24 +0700

    Update ProfileFragment.java

--- app/src/main/java/TugasAkhir/Mobile/movie/ProfileFragment.java
+++ app/src/main/java/TugasAkhir/Mobile/movie/ProfileFragment.java
@@ -10,12 +10,30 @@ import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
 
-    DatabaseHelper db;
-    Button logout;
+    private Button button;
 
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        button = view.findViewById(R.id.btn_logout);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent logout = new Intent(getActivity(), LoginActivity.class);
+                startActivity(logout);
+            }
+        });
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profil, container, false);
-
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_profil, container, false);
+        return view;
     }
 }
