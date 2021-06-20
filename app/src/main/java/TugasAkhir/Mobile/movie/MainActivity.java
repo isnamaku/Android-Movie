a21b8789e02cdd7dd37ba080a8462001f7e59b59
ganisdwpr <ganisprabandari80707@gmail.com>
2021-06-20 19:23:46 +0700

    ganis

--- app/src/main/java/TugasAkhir/Mobile/movie/MainActivity.java
+++ app/src/main/java/TugasAkhir/Mobile/movie/MainActivity.java
@@ -1,15 +1,37 @@
 package TugasAkhir.Mobile.movie;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    TextView textViewEmail, textViewLogout, textViewName, textViewPassword;
+
+    Button btnEditDetails;
+
+    private AlertDialog.Builder alertDialogBuilder;
+    private AlertDialog dialog;
+    private LayoutInflater inflater;
+
+    String textViewNameString;
+    String textViewEmailString;
+    String textViewPasswordString;
+    int textViewID;
 
     private BottomNavigationView main_navbar;
 
@@ -17,6 +39,43 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewName = findViewById(R.id.textViewName);
+        textViewEmail = findViewById(R.id.textViewEmail);
+        textViewPassword = findViewById(R.id.textViewPassword);
+        textViewLogout = findViewById(R.id.textViewLogout);
+
+        final Bundle b = getIntent().getExtras();
+
+        textViewID = Integer.parseInt(b.getString("textViewId"));
+        textViewNameString = b.getString("textViewUsername");
+        textViewEmailString = b.getString("textViewEmail");
+
+
+        Log.d("Data", String.valueOf(textViewID));
+        Log.d("Data", "name: " + textViewNameString);
+        Log.d("Data", "Email: " + textViewEmailString);
+        Log.d("Data", "password: " + textViewPasswordString);
+
+        textViewName.setText("Hello " + textViewNameString + "!");
+        textViewEmail.setText(textViewEmailString);
+        textViewPassword.setText(textViewPasswordString);
+
+        textViewLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+
+        btnEditDetails = findViewById(R.id.btnEditDetails);
+        btnEditDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editUserDidals();
+            }
+        });
 
         main_navbar = findViewById(R.id.main_navbar);
         main_navbar.setOnNavigationItemSelectedListener(this);
@@ -43,9 +102,53 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 fragment = new HomeFragment();
                 break;
             case R.id.profile:
-                fragment = new ProfileFragment();
+                fragment = new UpcomingFragment();
                 break;
         }
         return loadFragment(fragment);
+    }
+
+    public void editUserDidals() {
+        alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        inflater = LayoutInflater.from(MainActivity.this);
+        final View view = inflater.inflate(R.layout.popup, null);
+        final EditText editTextUsername = view.findViewById(R.id.editTextUsername);
+        final EditText editTextEmail = view.findViewById(R.id.editTextEmail);
+
+        alertDialogBuilder.setView(view);
+        dialog = alertDialogBuilder.create();
+        dialog.show();
+
+        final User user = new User();
+        user.setId(textViewID);
+        user.setUserName(textViewNameString);
+        user.setEmail(textViewEmailString);
+        user.setPassword(textViewPasswordString);
+
+        editTextUsername.setText(user.getUserName());
+        editTextEmail.setText(user.getEmail());
+
+        final DatabaseHelper db = new DatabaseHelper(MainActivity.this);
+
+        Button saveButton = view.findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.setUserName(editTextUsername.getText().toString());
+                user.setEmail(editTextEmail.getText().toString());
+
+                if (!editTextUsername.getText().toString().isEmpty()
+                        && !editTextEmail.getText().toString().isEmpty()) {
+                    db.updateUser(user);
+                    Snackbar.make(v, "Details Saved!", Snackbar.LENGTH_LONG).show();
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                } else {
+                    Snackbar.make(view, "Don't save ", Snackbar.LENGTH_LONG).show();
+                }
+                dialog.dismiss();
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/TugasAkhir/Mobile/movie/UpcomingFragment.java
@@ -0,0 +1,128 @@
+package TugasAkhir.Mobile.movie;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class UpcomingFragment extends Fragment {
+
+
+    ProgressDialog loading;
+    private List<MovieResult> moviePopularResult = new ArrayList<>();
+    BaseApiService mApiService;
+    public static final int CONNECTION_TIMEOUT = 10000;
+    public static final int READ_TIMEOUT = 15000;
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    public static final String API_KEY = "e6bb64e1c930a688fb64df291debff39";
+    public static final String LANGUAGE = "en-US";
+    private int currentPageMoviePopular = 1;
+    private int totalPagesMoviePopular = 1;
+    LinearLayoutManager linearLayoutManager;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_upcoming, container, false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Retrofit retrofit = ApiClient.getClient();
+        mApiService = retrofit.create(BaseApiService.class);
+        recyclerView = view.findViewById(R.id.rvMovie);
+        linearLayoutManager = new LinearLayoutManager(getContext());
+
+        setPopularMovies();
+
+    }
+
+    private void setPopularMovies() {
+        recyclerView = recyclerView.findViewById(R.id.rvMovie);
+        linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        Toast toast = Toast.makeText(getContext(), "Loading all upcoming movies", Toast.LENGTH_SHORT);
+        toast.show();
+        getPopularMovies();
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (!recyclerView.canScrollVertically(2)) {
+                    if (dy > 0) {
+                        if (currentPageMoviePopular <= 10) {
+                            currentPageMoviePopular += 1;
+                        }
+                    } else if(dy<0){
+
+                        currentPageMoviePopular -= 1;
+                        linearLayoutManager.getReverseLayout();
+
+                    }
+                    getPopularMovies();
+                }
+            }
+
+        });
+
+    }
+
+    private void getPopularMovies() {
+        Call<Respons> call = mApiService.getPopularMovies(API_KEY, LANGUAGE, currentPageMoviePopular);
+        call.enqueue(new Callback<Respons>() {
+            @Override
+            public void onResponse(@NonNull Call<Respons> call, @NonNull Response<Respons> response) {
+                if (response.body() != null) {
+                    totalPagesMoviePopular = response.body().getTotalPages();
+                    if (response.body().getResults() != null) {
+                        int oldCount = moviePopularResult.size();
+                        moviePopularResult.addAll(response.body().getResults());
+                        Log.d("Retrofit Get", "Number of movies : " +
+                                String.valueOf(moviePopularResult.size()));
+                        List<MovieResult> movies = response.body().getResults();
+                        recyclerView.setLayoutManager(linearLayoutManager);
+                        recyclerView.setAdapter(new MovieAdapter(movies, R.layout.row, getContext()));
+
+
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<Respons> call, Throwable t) {
+                Log.e("Retrofit Get", t.toString());
+            }
+        });
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
